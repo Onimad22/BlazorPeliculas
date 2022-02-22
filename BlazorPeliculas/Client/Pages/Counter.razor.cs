768bc3c607833535610d461383405ef86d26c90f
Marcos dalmo <marcosdalmolin@hotmail.com>
2022-02-22 17:22:50 -0400

    29.Clases parciales

---
+++ BlazorPeliculas/Client/Pages/Counter.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorPeliculas.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject] ServicioSingleton singleton { get; set; }
+        [Inject] ServicioTransient transient { get; set; }
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            singleton.Valor = currentCount;
+            transient.Valor = currentCount;
+        }
+    }
+}
