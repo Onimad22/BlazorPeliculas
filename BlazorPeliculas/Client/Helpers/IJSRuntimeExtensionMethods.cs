cdcf1f22c557654434f06354f4a0dd2dedb16b6d
Marcos dalmo <marcosdalmolin@hotmail.com>
2022-02-23 13:47:28 -0400

    31. Invocar Javascritp desde c#

---
+++ BlazorPeliculas/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> confirm(this IJSRuntime js, string mensage)
+        {
+            await js.InvokeVoidAsync("console.log", "prueba de console.log");
+            return await js.InvokeAsync<bool>("confirm", mensage);
+        }
+    }
+}
