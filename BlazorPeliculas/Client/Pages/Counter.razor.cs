c3368961a85316e04dda92e487f919c5923d51a5
Marcos dalmo <marcosdalmolin@hotmail.com>
2022-03-01 14:18:04 -0400

    45. explorando el App.razor

--- BlazorPeliculas/Client/Pages/Counter.razor.cs
+++ BlazorPeliculas/Client/Pages/Counter.razor.cs
@@ -7,33 +7,25 @@ namespace BlazorPeliculas.Client.Pages
 {
     public partial class Counter
     {
-        [Inject] ServicioSingleton singleton { get; set; }
-        [Inject] ServicioTransient transient { get; set; }
         [Inject] protected IJSRuntime JS { get; set; }
 
         IJSObjectReference modulo;
 
-        private int currentCount = 0;
+        protected int currentCount = 0;
         static int currentCountStatic = 0;
 
         [JSInvokable]
         public async Task IncrementCount()
         {
             modulo = await JS.InvokeAsync<IJSObjectReference>("import", "./js/Counter.js");
-            await modulo.InvokeVoidAsync("mostrarAlerta");
+            await modulo.InvokeVoidAsync("mostrarAlerta","hola mundo");
 
             currentCount++;
-            singleton.Valor = currentCount;
-            transient.Valor = currentCount;
             currentCountStatic++;
             await JS.InvokeVoidAsync("pruebaPuntoNetStatic");
         }
 
-        [JSInvokable]
-        public static Task<int> ObtenerCurrentCount()
-        {
-            return Task.FromResult(currentCountStatic);
-        }
+        
 
         protected async Task IncrementCountJavascript()
         {
